6b1ec4084227400dc41532146ac3415fe064b3c8
ligen <1165028742@qq.com>
2023-06-11 01:45:21 +0800

    第一次提交

---
+++ htmng/src/views/Main.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <el-container>
+      <el-aside style="overflow: hidden; width: 200px;">
+        <commen-aside/>
+      </el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>
+            <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+  <script>
+  import commenAside from '@/components/commenAside.vue';
+export default {
+  data() {
+    return {};
+  },
+  components:{
+    commenAside
+  }
+};
+</script>
+  
