c6089c7191cb60a49b9a7c79c7d88c3d33b0923a
ligen <1165028742@qq.com>
2023-06-14 21:05:37 +0800

    5

--- htmng/src/components/commenHeader.vue
+++ htmng/src/components/commenHeader.vue
@@ -2,14 +2,17 @@
   <div class="header-container">
     <div class="l-content">
       <el-button
+        style="margin-right: 20px;"
         @click="handleMenu()"
         size="mini"
         icon="el-icon-menu"
       ></el-button>
-      <span>首页</span>
       <el-breadcrumb separator="/">
-        <el-breadcrumb-item v-for="item in tabList" :key="item.name" :to="{ path: '/' }"
-          >首页</el-breadcrumb-item
+        <el-breadcrumb-item
+          v-for="item in tabList"
+          :key="item.name"
+          :to="{ path: item.path }"
+          >{{ item.label }}</el-breadcrumb-item
         >
       </el-breadcrumb>
     </div>
@@ -35,11 +38,31 @@
   justify-content: space-between;
   align-items: center;
 }
-.l-content span {
-  color: #fff;
-  font-size: 14px;
-  margin-left: 10px;
+.l-content {
+  display: flex;
+  align-items: center;
+  /deep/.el-breadcrumb__item{
+    &:last-child{
+      .el-breadcrumb__inner{
+        color: #fff;
+      }
+      
+    }
+  }
+  /deep/.el-breadcrumb__inner{
+    font-weight: normal;
+    color:#606266
+  }
+  .el-breadcrumb :hover{
+    // color:#fff
+  }
+  .l-content span {
+    color: #fff;
+    font-size: 14px;
+    margin-left: 10px;
+  }
 }
+
 .r-content {
   .user {
     width: 40px;
@@ -60,11 +83,11 @@ export default {
     },
   },
   computed: {
-  // 使用对象展开运算符将 getter 混入 computed 对象中
-    ...mapState([
-      'tabList',
-    ])
-  }
+    // 使用对象展开运算符将 getter 混入 computed 对象中
+    ...mapState({
+      tabList: (state) => state.tab.tabList,
+    }),
+  },
 };
 </script>
   
