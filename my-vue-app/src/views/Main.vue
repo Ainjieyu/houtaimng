55c773c4e7295ea292681c2244570309d1bb1fe1
ligen <1165028742@qq.com>
2023-06-20 15:14:14 +0800

    vue3

---
+++ my-vue-app/src/views/Main.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-header>
+            <commen-header/>
+        </el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+<script>
+import { defineComponent } from 'vue';
+import commenHeader from '../components/commenHeader.vue';
+export default defineComponent({
+    components:{
+        commenHeader,
+    }
+})
+</script>
